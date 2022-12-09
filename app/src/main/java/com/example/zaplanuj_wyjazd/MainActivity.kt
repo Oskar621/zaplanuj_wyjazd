fdc09df20e0815a315197b7e60e89f61d41dc8da
test <test@asd.pl>
2022-12-09 16:44:21 +0100

    Dodanie minimalnej daty i maksymalnej daty kalendarza

--- app/src/main/java/com/example/zaplanuj_wyjazd/MainActivity.kt
+++ app/src/main/java/com/example/zaplanuj_wyjazd/MainActivity.kt
@@ -1,11 +1,21 @@
  package com.example.zaplanuj_wyjazd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CalendarView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val kalendarz = findViewById<CalendarView>(R.id.kalendarz)
+
+        kalendarz.minDate = Date().time
+        kalendarz.maxDate = Date().time + 63072000000
+
+
     }
 }
