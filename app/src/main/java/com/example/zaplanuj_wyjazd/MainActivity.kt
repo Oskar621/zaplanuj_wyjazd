9849ae88e768cc75fc0876f51b13c608ddc771d2
test <test@asd.pl>
2022-12-09 17:03:31 +0100

    proby  do pozwolenia na zaznaczanie 2 roznych dat na jednym kalendarzu

--- app/src/main/java/com/example/zaplanuj_wyjazd/MainActivity.kt
+++ app/src/main/java/com/example/zaplanuj_wyjazd/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.CalendarView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.datepicker.MaterialDatePicker
 import java.util.*
 
  class MainActivity : AppCompatActivity() {
@@ -15,7 +16,23 @@ import java.util.*
 
         kalendarz.minDate = Date().time
         kalendarz.maxDate = Date().time + 63072000000
+        val dateRangePicker =
+            MaterialDatePicker.Builder
+                .dateRangePicker()
+                .setTitleText("Title")
+                .setSelection(
+                    Pair(
+                        MaterialDatePicker.thisMonthInUtcMilliseconds(),
+                        MaterialDatePicker.todayInUtcMilliseconds()
+                    )
+                )
+                .build()
 
+        dateRangePicker.show(childFragmentManager, "tag")
+        dateRangePicker.addOnPositiveButtonClickListener {
+            println(it.first)
+            println(it.second)
 
+        }
     }
 }
