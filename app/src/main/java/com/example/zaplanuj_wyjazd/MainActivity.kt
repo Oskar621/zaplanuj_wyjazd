7571ae0f23ac139ec2e099585744f4c39ae79d5d
test <test@asd.pl>
2022-12-16 16:47:51 +0100

    Dodano funkcje ktora liczy dni pomiedzy poczatkiem a koncem wyjazdu. Dodano funckje onclickListener na przyciski poczatek wyjazdu i na koniec. OnclickListenery przypisują tekstom pod nimi date poczatku lub wyjadzu i jesli obydwie rzeczy sa wypelnione, to liczy dni pomiedzy nimi

--- app/src/main/java/com/example/zaplanuj_wyjazd/MainActivity.kt
+++ app/src/main/java/com/example/zaplanuj_wyjazd/MainActivity.kt
@@ -5,14 +5,14 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.CalendarView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.get
-import java.text.DateFormat
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.math.absoluteValue
 
  class MainActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
+    @SuppressLint("MissingInflatedId", "SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -27,25 +27,57 @@ import java.util.*
         kalendarz.minDate = Date().time
         kalendarz.maxDate = Date().time + 63072000000
 
-        val selDate = arrayListOf<Int>(zmienNaDate(kalendarz.date)[0],zmienNaDate(kalendarz.date)[1],zmienNaDate(kalendarz.date)[2])
-        kalendarz.setOnDateChangeListener() { CalendarView,d,m,y ->
+        val firstDay = mutableListOf(0,0,0)
+        val lastDay = mutableListOf(0,0,0)
+        val selDate = arrayListOf(zmienNaDate(kalendarz.date)[0],zmienNaDate(kalendarz.date)[1],zmienNaDate(kalendarz.date)[2])
+        kalendarz.setOnDateChangeListener{ _, d, m, y ->
             selDate[0] = d
             selDate[1] = m+1
             selDate[2] = y
         }
         poczWyj.setOnClickListener {
+            if(lastDay[0] <= selDate[0] || lastDay[1] <= selDate[1] || lastDay[2] <= selDate[2]) for (i in 0 until 3)
+                firstDay[i] = selDate[i]
+            poczTxt.text = "Poczatek Wyjazdu: ${System.lineSeparator()}${firstDay[0]}-${firstDay[1]}-${firstDay[2]}"
+
+
+            if(lastDay[0] != 0 && firstDay[0] != 0)
+                if(firstDay[2] > lastDay[2] && firstDay[1] == lastDay[1])
+                    Toast.makeText(applicationContext, "Nie mozesz najpierw wrocic potem wyjechac", Toast.LENGTH_SHORT).show()
+                else
+                    policz(lastDay, firstDay, iloscDni)
         }
+
 
         konWyj.setOnClickListener {
+            for (i in 0 until 3)
+                lastDay[i] = selDate[i]
+            konTxt.text = "Koniec Wyjazdu: ${System.lineSeparator()}${lastDay[0]}-${lastDay[1]}-${lastDay[2]}"
 
+
+            if(lastDay[0] != 0 && firstDay[0] != 0)
+                if(firstDay[2] > lastDay[2] && firstDay[1] == lastDay[1])
+                    Toast.makeText(applicationContext, "Nie mozesz najpierw wrocic potem wyjechac", Toast.LENGTH_SHORT).show()
+                else
+                    policz(lastDay, firstDay, iloscDni)
         }
     }
-     fun zmienNaDate(czas : Long): List<Int> {
+
+     @SuppressLint("SetTextI18n")
+     private fun policz(lastDay: MutableList<Int>, firstDay : MutableList<Int>, TxtView : TextView) {
+         val departureDay = (firstDay[0]*360) + (firstDay[1]*30) + firstDay[2]
+         val backDay = (lastDay[0]*360) + (lastDay[1]*30) + lastDay[2]
+         val diff =  departureDay.toChar() - backDay.toChar()
+         TxtView.text = "Ilosc dni: ${System.lineSeparator()}${diff.absoluteValue+1}"
+     }
+
+     @SuppressLint("SimpleDateFormat")
+     private fun zmienNaDate(czas : Long): List<Int> {
          val date = Date(czas)
-         val dateFormat = SimpleDateFormat("dd/MM/yyyy")
+         val dateFormat = SimpleDateFormat("yyyy/MM/dd")
          val formDate = dateFormat.format(date).split("/").map {
              it.toInt()
          }
-         return formDate;
+         return formDate
      }
 }
